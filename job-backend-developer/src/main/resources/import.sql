55aae353bf42755482d1da2c0ea7a4196ed2add6
Andre Rocha <androch1977@gmail.com>
2018-06-24 14:44:07 -0300

    Atualização do arquivo import.sql

--- job-backend-developer/src/main/resources/import.sql
+++ job-backend-developer/src/main/resources/import.sql
@@ -1,4 +1,9 @@
+-- Clientes
 insert into Cliente (id, nome, email) values (1, 'Andre Rocha', 'and@email.com');
 insert into Cliente (id, nome, email) values (2, 'Carlos Rocha', 'carlos@email.com');
 insert into Cliente (id, nome, email) values (3, 'Rocha Rocha', 'rocha@email.com');
 insert into Cliente (id, nome, email) values (4, 'Rocha R', 'r@email.com');
+
+-- PROFILES
+insert into ed_profile(name, description, active) values('ROLE_ADMIN', 'Administração', 'true');
+insert into ed_profile(name, description, active) values('ROLE_CADASTRO', 'Cadastros', 'true');
