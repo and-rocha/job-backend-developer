ac619ecda944f71995e7d1784e3f2736a9fa386d
Andre Rocha <androch1977@gmail.com>
2018-06-23 22:25:11 -0300

    Arquivo import.sql

--- job-backend-developer/src/main/resources/import.sql
+++ job-backend-developer/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into Cliente (id, nome, email) values (1, 'André Rocha', 'and@email.com');
+insert into Cliente (id, nome, email) values (1, 'Andre Rocha', 'and@email.com');
 insert into Cliente (id, nome, email) values (2, 'Carlos Rocha', 'carlos@email.com');
 insert into Cliente (id, nome, email) values (3, 'Rocha Rocha', 'rocha@email.com');
 insert into Cliente (id, nome, email) values (4, 'Rocha R', 'r@email.com');
