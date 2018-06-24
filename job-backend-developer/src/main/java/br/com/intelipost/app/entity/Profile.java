4d31993f423e93803487f19b7f3c0cf35937704c
Andre Rocha <androch1977@gmail.com>
2018-06-24 14:25:06 -0300

    Implementação da classe Profile

---
+++ job-backend-developer/src/main/java/br/com/intelipost/app/entity/Profile.java
@@ -0,0 +1,72 @@
+package br.com.intelipost.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Profile implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length=64, nullable=false)
+	@Size(max=64)
+	private String name;
+	
+	@Column(length=64)
+	@Size(max=64)
+	private String description;
+	
+	@Column(nullable=false)
+	private Boolean active;
+
+	public Profile() {
+		super();
+	}
+	
+	public Profile(Integer id) {
+		super();
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+}
